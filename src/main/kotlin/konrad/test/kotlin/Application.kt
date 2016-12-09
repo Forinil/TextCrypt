4600c79453367bc754f35792e2fed82ab6a900da
Konrad Botor <kbotor@gmail.com>
2016-12-09 20:50:24 +0100

    Adding command line option parser

--- src/main/kotlin/konrad/test/kotlin/Application.kt
+++ src/main/kotlin/konrad/test/kotlin/Application.kt
@@ -1,5 +1,6 @@
 package konrad.test.kotlin
 
+import org.apache.commons.cli.*
 import javax.crypto.Cipher
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.DESKeySpec
@@ -8,25 +9,18 @@ import java.io.File
 import java.io.InputStream
 import java.util.*
 import javax.crypto.SecretKey
+import org.apache.commons.cli.HelpFormatter
+
+
+
 
 /**
  * Stworzone przez Konrad Botor dnia 2016-12-08.
  */
 fun main(args: Array<String>) {
-    val file: File?
-    var propertiesStream: InputStream? = null
-    val properties = Properties()
+    val properties = processOptions(args)
 
-    if (args.isNotEmpty()) {
-        file = File(args[0])
-        propertiesStream = file.inputStream()
-    }
-
-    if (propertiesStream == null) {
-        propertiesStream = Class.forName("konrad.test.kotlin.ApplicationKt").classLoader.getResourceAsStream("application.properties")
-    }
-
-    properties.load(propertiesStream)
+    if (properties.keys.contains("print_help")) return
 
     val algorithm = properties.getProperty("algorithm", "")
     val mode = properties.getProperty("mode", "")
@@ -48,6 +42,71 @@ fun main(args: Array<String>) {
     val decryptedText: String = decrypt(cipher, encryptedString, secretKey)
 
     println("Decrypted string: $decryptedText")
+}
+
+fun processOptions(args: Array<String>): Properties {
+    val file: File?
+    val properties = Properties()
+    val parser = DefaultParser()
+    val commandLine: CommandLine?
+    val propertiesStream: InputStream = Class.forName("konrad.test.kotlin.ApplicationKt").classLoader.getResourceAsStream("application.properties")
+
+    properties.load(propertiesStream)
+
+    val configurationFileOption = Option.builder("c").argName("path-to-configuration-file").longOpt("configuration_file").hasArg().required(false).optionalArg(false).desc("Configuration file path").build()
+    val textFileOption = Option.builder("f").argName("path-to-input-file").longOpt("input-file").hasArg().required(false).optionalArg(false).desc("Input file path").build()
+    val outFileOption = Option.builder("o").argName("path-to-output-file").longOpt("output-file").hasArg().required(false).optionalArg(true).desc("Output file path").build()
+    val helpOption = Option.builder("h").longOpt("help").hasArg(false).required(false).desc("Print this help").build()
+    val decryptOption = Option.builder("d").longOpt("decrypt").hasArg(false).required(false).desc("Decrypt input").build()
+    val encryptOption = Option.builder("e").longOpt("encrypt").hasArg(false).required(false).desc("Encrypt input").build()
+
+    val options = Options()
+    options.addOption(configurationFileOption)
+    options.addOption(textFileOption)
+    options.addOption(outFileOption)
+    options.addOption(helpOption)
+    options.addOption(decryptOption)
+    options.addOption(encryptOption)
+
+
+    try {
+        commandLine = parser.parse(options, args)
+    } catch (exp: ParseException) {
+        System.err.println("Parsing failed.  Reason: " + exp.message)
+        exp.printStackTrace()
+        return properties
+    }
+
+    if (commandLine.hasOption("h")) {
+        properties.setProperty("print_help", "true")
+        val formatter = HelpFormatter()
+        formatter.printHelp("TextCrypt", options)
+        return properties
+    }
+
+    if (commandLine.hasOption("c")) {
+        val path = commandLine.getOptionValue("c")
+        file = File(path)
+        properties.load(file.inputStream())
+    }
+
+    if (commandLine.hasOption("f")) {
+        properties.setProperty("input_file", commandLine.getOptionValue("f"))
+    }
+
+    if (commandLine.hasOption("o")) {
+        properties.setProperty("output_file", commandLine.getOptionValue("f"))
+    }
+
+    if (commandLine.hasOption("e")) {
+        properties.setProperty("encrypt", "true")
+    } else if (commandLine.hasOption("d")) {
+        properties.setProperty("decrypt", "true")
+    } else {
+        properties.setProperty("encrypt", "true")
+    }
+
+    return properties
 }
 
 private fun decrypt(cipher: Cipher, encryptedString: String, secretKey: SecretKey?): String {
